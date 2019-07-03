5353163fad918db30024239c5c557ba74126c38a
Administrator <405886056@qq.com>
2019-07-03 20:58:42 +0800

    modify

--- pachong2.py
+++ pachong2.py
@@ -10,15 +10,17 @@ headers = {
     "referer": "https://www.mzitu.com/tag/ugirls"
 }
 
-for i in range(1, 64):
+url = 'https://www.mzitu.com/186386'
+
+for i in range(1, 16):
     if i == 1 :
-        response = requests.get("https://www.mzitu.com/185653", headers=headers)
+        response = requests.get(url, headers=headers)
     else:
-        response = requests.get("https://www.mzitu.com/185653/" + str(i), headers=headers)
+        response = requests.get(url+ '/'+ str(i), headers=headers)
     html = etree.HTML(response.content)
     img_info = html.xpath('/html/body/div[2]/div[1]/div[3]/p/a/img/@src')
     img_src = img_info[0]
-    # print(img_src)
+    print(img_src)
     fileName = "mzitu\\" + str(i) + ".jpg"
     with open(fileName, "wb") as f:
         response = requests.get(img_src, headers=headers)
