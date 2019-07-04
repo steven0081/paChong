c5fedf2a3ced78e43d052ba7666159444dfd44c6
joe <405886056@qq.com>
2019-07-04 15:24:48 +0800

    modify

--- pachong2.py
+++ pachong2.py
@@ -10,9 +10,9 @@ headers = {
     "referer": "https://www.mzitu.com/tag/ugirls"
 }
 
-url = 'https://www.mzitu.com/186386'
+url = 'https://www.mzitu.com/187283'
 
-for i in range(1, 16):
+for i in range(1, 42):
     if i == 1 :
         response = requests.get(url, headers=headers)
     else:
