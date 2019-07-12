10fce46d62ff7322550b1467ca97e4761a01a50c
Administrator <405886056@qq.com>
2019-07-12 23:42:57 +0800

    modify

--- pa_video.py
+++ pa_video.py
@@ -9,19 +9,23 @@ headers={
                   " Chrome/63.0.3239.132 Mobile Safari/537.36"
 }
 
-def video_download(title,href):
+#下载视频文件
+def  video_download(title, href):
     url = 'https://www.pearvideo.com/'+ href
-    print(url)
     brower = webdriver.Chrome()
     brower.get(url)
     time.sleep(1)
     video_src = brower.find_element_by_xpath('//*[@id="JprismPlayer"]/video').get_attribute('src')
-    #print(item.get_attribute('src'))
+    brower.close()
+    print(title, url, video_src)
+    title = str(title)[0:4]
     fileName = "video\\" + title + ".mp4"
     with open(fileName, "wb") as f:
-        response = requests.get(video_src, headers=headers)
-        f.write(response.content)
-    brower.close()
+        try:
+            response = requests.get(video_src, headers=headers)
+            f.write(response.content)
+        except Exception as e:
+            print(e)
 
 url = "https://www.pearvideo.com/category_1"
 
@@ -31,4 +35,4 @@ videotitle_list = html.xpath('//*[@id="categoryList"]/li[*]/div/a/div[2]')
 videohref_list = html.xpath('//*[@id="categoryList"]/li[*]/div/a/@href')
 for t ,h in zip(videotitle_list, videohref_list):
     video_download(t.text, h)
-    print(t.text,h)
+    #print(t.text,h)
