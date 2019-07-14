819f017e4c7cf4236184c58fbbb6ac38d8db77f1
Administrator <405886056@qq.com>
2019-07-15 00:21:17 +0800

    modify

--- pa_books.py
+++ pa_books.py
@@ -1,17 +1,34 @@
 import requests
 from lxml import etree
-import os
+import time
 
 headers = {
-    "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36"
-                  " (KHTML, like Gecko) Chrome/63.0.3239.132 Mobile Safari/537.36",
-    "referer": "https://www.95590.org/"
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+                  " Chrome/75.0.3770.100 Safari/537.36",
+    "referer": "https://www.95590.org/",
+    "keep-live": "false"
 }
-
+def get_content(title, charpter, src):
+    try:
+        response = requests.get(src, headers=headers)
+        html = etree.HTML(response.content)
+        src_list = html.xpath('//div[@class="entry-content"]/p')
+        #print(len(src_list))
+        # for src in zip(src_list):
+        content = src[0].text
+        file_name = '.\\books\\' + title + '.txt'
+        with open(file_name, 'a') as f:
+            f.write(content + '\n')
+        #print(content)
+        response.close()
+    except Exception as e:
+        print(e)
+    time.sleep(1)
+    
 def get_charpter(title, src):
     response = requests.get(src, headers=headers)
     html = etree.HTML(response.content)
-    src_list = html.xpath('//*[@id="category-64"]/div/ul/li[*]/a')
+    src_list = html.xpath('//div[@class="entry-content"]/ul/li[*]/a')
     #print(len(src_list))
     for src in zip(src_list):
         charpter = src[0].text
@@ -19,22 +36,14 @@ def get_charpter(title, src):
         print(charpter, content_src)
         file_name = '.\\books\\' + title + '.txt'
         with open(file_name, 'a') as f:
-            f.write(charpter+'\n')
+           f.write(charpter+'\n')
+        time.sleep(3)
+        response.close()
         get_content(title, charpter, content_src)
 
-def get_content(title, charpter, src):
-    response = requests.get(src, headers=headers)
-    html = etree.HTML(response.content)
-    src_list = html.xpath('//div[@class="entry-content"]/p')
-    #print(len(src_list))
-    for src in zip(src_list):
-        content = src[0].text
-        with open(file_name, 'a') as f:
-            f.write(content+'\n')
-        #print(content)
 
 # 主程序入口， 先爬取官场小说
-response= requests.get("https://www.95590.org/",headers=headers)
+response= requests.get("https://www.95590.org/", headers=headers)
 html = etree.HTML(response.content)
 title_list = html.xpath('//*[@id="categories"]/ul/li[*]/a/text()')
 src_list =  html.xpath('//*[@id="categories"]/ul/li[*]/a/@href')
@@ -44,8 +53,10 @@ for title, src in zip(title_list, src_list):
     print(title, src)
     file_name = '.\\books\\'+title+'.txt'
     with open(file_name, 'w') as f:
-        f.write(title)
+        f.write(title + '\n')
+    response.close()
     get_charpter(title, src)
+
 
 
 
